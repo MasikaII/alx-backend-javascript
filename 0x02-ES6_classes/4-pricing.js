1bd83bf824ded1b9b848b52282da103350b22b51
Stephy <atienostephanie040@gmail.com>
2023-03-31 10:10:36 +0000

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount = 0, currency = Currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+export default Pricing;
